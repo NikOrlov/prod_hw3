1ba97bac2450afd345e033700dbe28cbf9b83d05
lolvista <nikovtb1@gmail.com>
2021-06-11 15:59:33 +0300

    update utils (email settings)

--- dags/utils.py
+++ dags/utils.py
@@ -4,9 +4,11 @@ from airflow.utils.dates import timedelta
 VOLUME = '/home/lolvista/MADE/Prod/prod_hw3/data:/data'
 
 default_args = {
-    "owner": "airflow",
-    "email": ["n1@gmail.com"],
-    "retries": 1,
-    "retry_delay": timedelta(minutes=5),
-    "email_on_failure": True
+    'owner': 'airflow',
+    'email': ['n1@gmail.com'],
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+    'email_on_failure': True,
+    'email_on_success': True,
+    'email_on_retry': True
 }
