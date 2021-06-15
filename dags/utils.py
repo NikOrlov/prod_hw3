3fa4fb428ae0a26e2934fbdb0a88bffa28947054
NikOrlov <nikovtb1@gmail.com>
2021-06-15 21:02:58 +0300

    change volume and email for production

--- dags/utils.py
+++ dags/utils.py
@@ -1,14 +1,12 @@
 from airflow.utils.dates import timedelta
 
 
-VOLUME = '/home/lolvista/MADE/Prod/prod_hw3/data:/data'
+VOLUME = '/your/abs/path/to/data:/data'
 
 default_args = {
     'owner': 'airflow',
-    'email': ['n1@gmail.com'],
+    'email': ['your_email@gmail.com'],
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
-    'email_on_failure': True,
-    'email_on_success': True,
-    'email_on_retry': True
+    'email_on_failure': True
 }
